1d151b497c36468b4a2161fa86dcfcb8b17dcc84
SerchPer11 <paso220489@upemor.edu.mx>
2026-03-28 23:36:21 -0600

    fix(cors): restrict allowed origins to frontend URL fix(.gitignore): add .env to ignore list fix(axios): update baseURL to use environment variable

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost/api', // La URL de tu API en Laravel
+    baseURL: import.meta.env.VITE_API_URL, // La URL de tu API en Laravel
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
